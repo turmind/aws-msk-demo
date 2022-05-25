4e8dcdf81940dc840396b150ae89c11d75175ca5
jdhuang <jdhuang@amazon.com>
2022-05-25 22:16:46 +0800

    init

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,64 @@
+package com.mycompany.app;
+
+//import util.properties packages
+import java.util.Properties;
+
+//import simple producer packages
+import org.apache.kafka.clients.producer.Producer;
+
+//import KafkaProducer packages
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+//import ProducerRecord packages
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+//Create java class named “SimpleProducer”
+public class App {
+   
+   public static void main(String[] args) throws Exception{
+
+      //Assign topicName to string variable
+      String topicName = "msk-serverless-tutorial";
+      
+      // create instance for properties to access producer configs   
+      Properties props = new Properties();
+      
+      //Assign localhost id
+      props.put("bootstrap.servers", "boot-srtdayv0.c2.kafka-serverless.ap-northeast-1.amazonaws.com:9098");
+      
+      //Set acknowledgements for producer requests.      
+      props.put("acks", "all");
+      
+      //If the request fails, the producer can automatically retry,
+      props.put("retries", 0);
+      
+      //Specify buffer size in config
+      props.put("batch.size", 16384);
+      
+      //Reduce the no of requests less than 0   
+      props.put("linger.ms", 1);
+      
+      //The buffer.memory controls the total amount of memory available to the producer for buffering.   
+      props.put("buffer.memory", 33554432);
+      
+      props.put("key.serializer", 
+         "org.apache.kafka.common.serialization.StringSerializer");
+         
+      props.put("value.serializer", 
+         "org.apache.kafka.common.serialization.StringSerializer");
+
+      props.put("security.protocol", "SASL_SSL");
+      props.put("sasl.mechanism", "AWS_MSK_IAM");
+      props.put("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
+      props.put("sasl.client.callback.handler.class", "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+      
+      Producer<String, String> producer = new KafkaProducer
+         <String, String>(props);
+            
+      for(int i = 0; i < 10; i++)
+         producer.send(new ProducerRecord<String, String>(topicName, 
+            Integer.toString(i), Integer.toString(i)));
+               System.out.println("Message sent successfully");
+               producer.close();
+    }
+}
